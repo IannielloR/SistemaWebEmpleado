e4ef6075ea2e920d805450976ef735aa12d96d35
IannielloR <rianniello@fi.uba.ar>
2023-01-31 16:50:03 -0300

    Modelo

--- SistemaWebEmpleado/Data/DbEmpleadoContext.cs
+++ SistemaWebEmpleado/Data/DbEmpleadoContext.cs
@@ -1,6 +1,12 @@
-﻿namespace SistemaWebEmpleado.Data
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaWebEmpleado.Models;
+
+namespace SistemaWebEmpleado.Data
 {
-    public class DbEmpleadoContext
+    public class DbEmpleadoContext: DbContext
     {
+        public DbEmpleadoContext(DbContextOptions<DbEmpleadoContext> options) : base(options) { }
+
+        public DbSet<Empleado> Empleados { get; set; }
     }
 }
